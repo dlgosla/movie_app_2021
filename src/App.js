74423c5824a0b71e52c389f22482c85b454109ab
dlgosla <dlgosla@gmail.com>
2021-06-29 10:56:36 +0900

    dynamic fav food

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//컴포넌트 재사용가능
+//컴포넌트로 정보를 보낼 수 있음
+//jsx = html + js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Food({name, picture}){ //리액트 매직 {fav} = props.fav
+  return <div>
+    <h1>i like {name}</h1>;
+    <img src={picture} />
+
     </div>
+}
+
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+  },
+  {
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+  },
+  {
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+];
+
+//map 은 랜더링을 함
+//friends.map(current => {console.log(current); return 0})
+//function(friend) {return friend + " gg";}
+function App() {
+    return (
+      //dish는 오브젝트
+    <div> 
+      {foodILike.map(dish => <Food name={dish.name} picture={dish.image} />)}
+      
+    </div> //부모 컴포넌트에서 자식 컴포넌트로 많은 프로퍼티를 보낼 수 있음
+
   );
+
+  /*
+  return (
+    <div> 
+      <h1>Hello!!!!!</h1>
+      < Food fav="kimchi" /> 
+      < Food fav="ramen" /> 
+      < Food fav="samgiopsal" /> 
+      < Food fav="chukumi" /> 
+    </div> //부모 컴포넌트에서 자식 컴포넌트로 많은 프로퍼티를 보낼 수 있음
+
+  );
+  */
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
+// 컴포넌트 : html을 반환하는 함수
+
 ReactDOM.render(
+  //컴포넌트를 사용할 때 쓰는 방법
+  //리액트는 컴포넌트를 사용해서  html처럼 작성하려는 경우에 팔요
+  // 자바스크립트와 html 사이의 이런 조합을 jsx라고 부름
+
+  //리액트 어플리케이션은 한번에 하나의 컴포넌트만 랜더링할 수 있음
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('potato')
 );
 
 
